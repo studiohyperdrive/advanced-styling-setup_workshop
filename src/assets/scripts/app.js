185e5ca86bc5010c5c40dd1bf3dbb4df5583efe1
Fabian Meul <fabian.meul@gmail.com>
2019-02-14 17:23:50 +0100

    feat: dark-mode

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -1,3 +1,32 @@
+let activeTheme = "themeLight";
+
+// This needs to happen before the DOM renders to prevent flickering
+setActiveStyleSheet(activeTheme);
+
 window.addEventListener("load", function (event) {
-	console.log("All resources finished loading!");
+	// console.log("All resources finished loading!");
+	init();
 });
+
+function init() {
+	const themeToggleButton = document.querySelector(".theme-toggle-btn");
+
+	themeToggleButton.addEventListener("click", () => {
+		activeTheme = activeTheme === "themeLight" ? "themeDark" : "themeLight";
+		setActiveStyleSheet(activeTheme);
+	});
+}
+
+function setActiveStyleSheet(title) {
+	const linkElements = document.getElementsByTagName("link");
+
+	for (let i = 0; i < linkElements.length; i++) {
+		const linkEl = linkElements[i];
+
+		linkEl.disabled = true;
+
+		if (linkEl.getAttribute("rel").indexOf("style") !== -1 && linkEl.getAttribute("title")) {
+			linkEl.disabled = !(linkEl.getAttribute("title") === title);
+		}
+	}
+}
