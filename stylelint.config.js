bbd1408a2d78f1b69323e12286795d9026238caa
Enzo Eghermanne <enzo.eghermanne@gmail.com>
2019-03-04 14:44:37 +0100

    chore: typography values in globals

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,5 +2,8 @@ module.exports = {
 	extends: [
 		"@studiohyperdrive/stylelint-config",
 		"@studiohyperdrive/stylelint-config/lib/sass"
-	]
+	],
+	rules: {
+		"sh-waqar/declaration-use-variable": [["/color/", "z-index", "font-size", { ignoreValues: ["transparent", "inherit", "initial", "/rem/", "/layer/"] }]],
+	}
 };
